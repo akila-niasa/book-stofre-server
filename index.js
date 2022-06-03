045ccada08c605a438090fd3cc4f4595169825e2
akila-niasa <akilaniasa@gmail.com>
2022-06-03 22:29:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const cors = require('cors');
+
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+// db_book_user
+// Z9GksubURt2Mw0gN
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bsuux.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
+async function run() {
+   
+    try{
+        await client.connect()
+        const booksCollection = client.db("bookdb").collection("books");
+        const orderList = client.db("bookdb").collection("orders");
+
+        //(GET)Get all books API database code
+  app.get('/books', (req, res) => {
+    booksCollection.find()
+    .toArray((err, book) => {
+      res.send(book)
+    })
+  })
+
+// (POST)Post order
+  app.post('/addOrder', (req, res) => {
+    const newOrder = req.body;
+    console.log(newOrder)
+    orderList.insertOne(newOrder)
+  .then(result => {
+    console.log(result)
+    res.send(result.insertedCount > 0);
+  })
+})
+
+// order books API database code
+app.get('/orders', (req, res) => {
+    orderList.find({})
+    .toArray((err, documents) => {
+        res.send(documents);
+    })
+  })
+  
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome in My CarRepair Server side')
+})
+app.listen(port, () => { console.log('CURD is running', port) })
